c58bad1480c8723959166326a347129f3e9ba93d
Dhanush CK <dhanushck01@gmail.com>
2025-01-05 17:48:14 +0530

    feat : Created an interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return(
+    <div className="container h-screen w-screen flex justify-center items-center flex-col">
+      <div className="container-element w-full h-full p-10 flex flex-col items-center">
+        <header className="header flex justify-center items-center text-5xl w-fit p-3 rounded-full text-black bg-white ">
+          Recipe
+        </header>
+        <div className="menu w-full h-full p-5 flex justify-center items-center">
+          
+          <div className="item">
+            <div className="item-header">
+              <h2>Non-Veg</h2>
+            </div>
+            <ul className="list">
+              <li className="list-item">Chicken</li>
+              <li className="list-item">Fish</li>
+              <li className="list-item">Mutton</li>
+            </ul>
+          </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <div className="item">
+            <div className="item-header">
+              <h2>Vegetarian</h2>
+            </div>
+            <ul className="list">
+              <li className="list-item">Paneer</li>
+              <li className="list-item">Cauliflower</li>
+              <li className="list-item">Potato</li>
+            </ul>
+          </div>
+
+          <div className="item">
+            <div className="item-header">
+              <h2>Desserts</h2>
+            </div>
+            <ul className="list">
+              <li className="list-item">Cake</li>
+              <li className="list-item">Pudding</li>
+              <li className="list-item">Cookies</li>
+            </ul>
+          </div>
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
