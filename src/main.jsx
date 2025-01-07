785f982910f355fef9d929dcb8b506392240c11a
Dhanush CK <dhanushck01@gmail.com>
2025-01-07 22:56:14 +0530

    feat : Setup of router path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './components/Home.jsx'
+import Chicken from './components/non-veg/Chicken.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path:"/chicken",
+        element: <Chicken />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
