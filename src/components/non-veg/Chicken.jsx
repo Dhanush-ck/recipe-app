8d99de69ec3e16703d5b57e0aa6419843f98aa47
Dhanush CK <dhanushck01@gmail.com>
2025-01-08 23:04:56 +0530

    feat : Added options

--- src/components/non-veg/Chicken.jsx
+++ src/components/non-veg/Chicken.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import cb from '../../assets/img/food-items/chicken-biriyani.jpg'
+import bc from '../../assets/img/food-items/butter-chicken.jpg'
+import cc from '../../assets/img/food-items/chilly-chicken.jpg'
 
 function Chicken() {
     return(
-        <div>
-            <h2>Chicken</h2>
+        <div className="item-container h-screen w-screen flex justify-center items-center flex-col bg-slate-300 text-black">
+            <div className="item-container-elements h-full w-full flex justify-center items-center gap-7 p-40">
+                <div className="dish">
+                    <img src={cb} alt="Chicken Biriyani"/>
+                    <div className="dish-info">
+                        <h2>Chicken Biriyani</h2>
+                    </div>
+                </div>
+                <div className="dish">
+                    <img src={bc} alt="Butter Chicken" />
+                    <div className="dish-info">
+                        <h2>Butter Chicken</h2>
+                    </div>
+                </div>
+                <div className="dish">
+                    <img src={cc} alt="Chilly Chicken" />
+                    <div className="dish-info">
+                        <h2>Chilly Chicken</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
