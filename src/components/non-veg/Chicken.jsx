7214acc427e654892cffe4fc661d6dbdc34235ae
Dhanush CK <dhanushck01@gmail.com>
2025-01-12 21:53:59 +0530

    refactor : moved tailwind style classes to css file

--- src/components/non-veg/Chicken.jsx
+++ src/components/non-veg/Chicken.jsx
@@ -5,10 +5,10 @@ import cc from '../../assets/img/food-items/chilly-chicken.jpg'
 
 function Chicken() {
     return(
-        <div className="item-container h-screen w-screen flex justify-center items-center flex-col bg-slate-300 text-black">
-            <div className="item-container-elements h-full w-full flex justify-center items-center flex-col p-40">
+        <div className="item-container ">
+            <div className="item-container-elements ">
                 <h2 className="text-4xl">Chicken Items</h2>
-                <div className="dish-holder flex justify-center items-center gap-7 ">
+                <div className="dish-holder ">
                     <div className="dish">
                         <img src={cb} alt="Chicken Biriyani"/>
                         <div className="dish-info">
